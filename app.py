0da6fc52bc1f64e4dec49a2b14cb6065ebb387d8
JosephDucrocq <ducrocq.joseph@gmail.com>
2025-04-07 15:40:12 +0100

    routes

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/concept', methods=['GET'])
+def concept():
+    return render_template('concept.html')
+
+@app.route('/merch', methods=['GET'])
+def merch():
+    return render_template('merch.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=int(os.environ.get("PORT", 5001)))
